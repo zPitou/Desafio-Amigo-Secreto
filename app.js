14d67e2ef2eee74f00c61351a6700b98d6974411
zPitou <nicolas.aragao.g@gmail.com>
2025-02-02 15:46:28 -0300

    Adicionar Amigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarNomes() {
+    let input = document.getElementById('amigo');
+    let nomes = input.value;
+
+    if (nomes === "") {
+        alert('Por favor, insira um nome');
+    } else {
+        amigos.push(nomes);
+        input.value = "";
+        console.log(amigos)
+    }
+}
