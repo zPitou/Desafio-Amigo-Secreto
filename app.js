cfba6070ede965fe97e4f0d62680695f8599a019
zPitou <nicolas.aragao.g@gmail.com>
2025-02-03 21:49:54 -0300

    Atualizar lista

--- app.js
+++ app.js
@@ -12,3 +12,14 @@ function adicionarNomes() {
         console.log(amigos)
     }
 }
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos')
+    lista.innerHTML = ''
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
